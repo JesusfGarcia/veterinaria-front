74ba3eee9d76e7be266966c55f54d0c187278c7d
JesusGarciaPixa <123981276+JesusGarciaPixa@users.noreply.github.com>
2023-02-08 18:51:32 -0700

    search icon added

--- src/views/stock/index.jsx
+++ src/views/stock/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Content from "../../components/content";
 
+import SearchIcon from "@mui/icons-material/Search";
+
 console.log("hola mundo desde la pantalla de  el inventario");
 export default function StockScreen() {
   return (
@@ -8,7 +10,7 @@ export default function StockScreen() {
       <div className="linea"></div>
       <div onkeydown="buscar(event)" class="input">
         <input placeholder="Buscar Producto" type="text" />
-        <img alt="lupa icon" src="./lupa.png" />
+        <SearchIcon />
       </div>
     </Content>
   );
