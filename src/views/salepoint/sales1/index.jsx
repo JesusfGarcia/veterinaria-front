9aaef718ee6b46354d898b2387e534957e53c1b9
anna98soto <annasotoochoa123@gmail.com>
2023-03-09 15:55:44 -0700

    sales1 arreglado

--- src/views/salepoint/sales1/index.jsx
+++ src/views/salepoint/sales1/index.jsx
@@ -1,66 +1,54 @@
 import React from "react";
 import Modal from "../../../components/dialog";
 import Table from "../../../components/table";
-export default function Sales2() {
-    const [isOpen, setIsOpen] = React.useState(false);
-}  
-const titles = [
+export default function Sales1() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const titles = [
     {
       label: "Concepto",
       key: "concept",
     },
     {
-        label: "Cantidad",
-        key: "quantity",
-      },
+      label: "Cantidad",
+      key: "quantity",
+    },
     {
       label: "Fecha",
       key: "Date",
     },
-  
-    
+
     {
       label: "Autorizó",
       key: "authorize",
     },
-  
-    
   ];
   const data = [
     {
-     concept:"Retiro de efectivo",
+      concept: "Retiro de efectivo",
       quantity: "2",
-      date:"12/03/23",
-      authorize:"mvz.Paola",
+      date: "12/03/23",
+      authorize: "mvz.Paola",
     },
     {
-    concept:"Retiro de efectivo",
-     quantity: "2",
-     date:"12/03/23",
-    authorize:"mvz.Paola",
+      concept: "Retiro de efectivo",
+      quantity: "2",
+      date: "12/03/23",
+      authorize: "mvz.Paola",
     },
-       
   ];
   const buttonConf = {
     label: "Añadir Movimiento",
-    onClick:()=> alert("hola mundo")
+    onClick: () => alert("hola mundo"),
   };
   const onClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
-  
-export default function Sales1() {
   return (
     <div>
-      <Table  buttonConf={buttonConf} columns={titles} data={data} />
-<Modal 
- title="Hola"
- isOpen={isOpen}
- onClose={onClose}>
-
-</Modal>
-     
+      <Table buttonConf={buttonConf} columns={titles} data={data} />
+      <Modal title="Hola" isOpen={isOpen} onClose={onClose}></Modal>
     </div>
   );
 }
