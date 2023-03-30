336e405fc64f827003d97d514df82c9e883b7acb
JesusGarciaPixa <123981276+JesusGarciaPixa@users.noreply.github.com>
2023-03-30 13:52:09 -0700

    pagination on userlist

--- src/views/configuration/reducer/index.js
+++ src/views/configuration/reducer/index.js
@@ -6,26 +6,6 @@ export const reducer = (state = initialState, action) => {
   const newState = structuredClone(state);
 
   switch (action.type) {
-    case actions.GET_DATA:
-      newState.isLoading = true;
-      newState.errorText = "";
-      break;
-    case actions.GET_DATA_SUCCESS:
-      newState.isLoading = false;
-      const totalPages = Math.ceil(action.payload.count / state.pageSize);
-      newState.count = totalPages;
-      console.log("payload", action.payload);
-      newState.list = action.payload.rows.map((item) => {
-        return {
-          ...item,
-          type: item.isAdmin ? "Administrador" : "Empleado",
-        };
-      });
-      break;
-    case actions.GET_DATA_ERROR:
-      newState.isLoading = false;
-      newState.errorText = action.payload;
-      break;
     case actions.OPEN_MODAL:
       newState.showModal = true;
       break;
@@ -64,12 +44,7 @@ export const reducer = (state = initialState, action) => {
     case actions.CLOSE_DELETE_MODAL:
       newState.showDeleteModal = false;
       break;
-    case actions.HANDLE_FILTER_TEXT:
-      newState.filterText = action.payload;
-      break;
-    case actions.CHANGE_PAGE:
-      newState.page = action.payload;
-      break;
+
     default:
       break;
   }
