6daf7ee877c603397e8d2b74542805bd2c25a434
JesusfGarcia <jesusblastoise99@gmail.com>
2023-04-18 15:02:15 -0700

    fecha de nacimiento con edad

--- src/helpers/getFormatedDate.js
+++ src/helpers/getFormatedDate.js
@@ -21,6 +21,26 @@ export const getTodayDate = () => {
   return today.split("T")[0];
 };
 
+export const IntToDate = (number) => {
+  //transformar un numero a una fecha
+  const today = new Date();
+  let dateString = today.toISOString().split("T")[0];
+  const todayYear = today.getFullYear();
+  const birthyear = todayYear - number;
+  let newDate = dateString.split("-");
+  const formatedString = `${birthyear}-${newDate[1]}-${newDate[2]}`;
+
+  return formatedString;
+};
+
+export const DateToInt = (date) => {
+  //calcular cuantos años han pasado desde la fecha input y devolver el numero
+  const today = new Date();
+  const birthday = new Date(date);
+  const age = today.getFullYear() - birthday.getFullYear();
+  return age;
+};
+
 const month = [
   "Enero",
   "Febrero",
